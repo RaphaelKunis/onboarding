5590a30d3d54b56cdccb220d7477a8f9a3aa3756
RaphaelKunis <krap81@gmail.com>
2021-08-15 21:32:27 +0200

    started with step 3

---
+++ spring-boot-mysql/src/main/java/de/raphaelkunis/springbootmysql/UserRepository.java
@@ -0,0 +1,8 @@
+package de.raphaelkunis.springbootmysql;
+
+import org.springframework.data.repository.CrudRepository;
+// import de.raphaelkunis.springbootmysql.User;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+public interface UserRepository extends CrudRepository<User, Integer> {}
